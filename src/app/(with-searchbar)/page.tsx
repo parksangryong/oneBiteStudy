d856a8595deccf9b3db463e6ef46e324da79db66
parksangryong <ppsr3478@naver.com>
2025-12-12 11:22:32 +0900

    fetching

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,21 +1,55 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
-import books from "@/mock/books.json";
+import { BookData } from "@/types/types";
 
-export default function Page() {
+async function AllBooks() {
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/book`);
+    const allBooks: BookData[] = await response.json();
+
+    return (
+      <div>
+        {allBooks.map((book) => (
+          <BookItem key={`reco-${book.id}`} {...book} />
+        ))}
+      </div>
+    );
+  } catch (error) {
+    console.error(error);
+    return <div>에러가 발생했습니다.</div>;
+  }
+}
+
+async function RecoBooks() {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/book/random`
+    );
+    const recoBooks: BookData[] = await response.json();
+
+    return (
+      <div>
+        {recoBooks.map((book) => (
+          <BookItem key={`reco-${book.id}`} {...book} />
+        ))}
+      </div>
+    );
+  } catch (error) {
+    console.error(error);
+    return <div>에러가 발생했습니다.</div>;
+  }
+}
+
+export default async function Page() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={`reco-${book.id}`} {...book} />
-        ))}
+        <RecoBooks />
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={`all-${book.id}`} {...book} />
-        ))}
+        <AllBooks />
       </section>
     </div>
   );
